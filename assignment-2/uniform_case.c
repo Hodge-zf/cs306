95cc23d5f1bbd7dc895804a2a6afada251e8b75d
Hodge-zf <xx.skcs@gmail.com>
2020-03-02 16:20:10 -0600

    Good function parameter passes

--- assignment-2/uniform_case.c
+++ assignment-2/uniform_case.c
@@ -1,36 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #define FALSE 0
 #define TRUE 1
 
-//void change_case(char * string[]);
+void change_case(int length, char * string[]);
 
 int main(int argc, char * argv[]){
 
-//  change_case($1);
-  for(int i = 1; i < argc; i++){
-    printf("%s", argv[i]);
-  }
+  int length = strlen(argv[1]);
+
+  change_case(length, argv);
+
+  //for(int i = 1; i < argc; i++){
+  printf("%s\n", argv[1]);
+  //}
 
   return 0;
 }
 
-/* void change_case(char string[]){
+void change_case(int length, char * string[]){
 
-//    int current_index = 0;
-    int current_char = 0;
+  int current_index = 0;
+  int current_char = 0;
 
+  printf("%d", length);
+  char * input_buffer = (char *)malloc(length * sizeof(char *));
+
+  if(input_buffer == NULL){
+    printf("%s", "Malloc failure, exiting...\n");
+    exit;
+  }
+  else{
+    printf("%s", "\nmalloc success\n");
+  }
+
+/*
     while(current_char != -1){
-      while(1){
-        current_char = fgetc(string);
+      while(current_index < length){
+        current_char = input;
 
         if(current_char == '\0'){ break; }
 
         else{
-          printf("%s", );
+          printf("%d", current_char);
         }
       }
-    }
-} */
+    } */
+}
