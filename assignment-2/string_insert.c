077b04781361af343b1180e820eb5062394d61b2
Hodge-zf <xx.skcs@gmail.com>
2020-03-04 15:48:18 -0600

    Runs, needs error message on out of bounds

--- assignment-2/string_insert.c
+++ assignment-2/string_insert.c
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <ctype.h>
 
+#define TRUE 1
+
+int get_str_len(char * string);
+
 int main(int argc, char * argv[]){
 
   if(argc != 4){
@@ -14,13 +18,52 @@ int main(int argc, char * argv[]){
   char * string1 = argv[1];
   char * string2 = argv[3];
   int insertion_index = atoi(argv[2]);
-  
-  printf("%s",string1);
+
+  //subtracting off 1 char for the two terminator chars
+  //might cause issue down the road, delete if so
+  char * final_string = (char *)malloc((sizeof(string1)*sizeof(string2)-sizeof(char)));
+
+  if(final_string == NULL){
+    fprintf(stderr, "Malloc failure, exiting...\n");
+    exit(EXIT_FAILURE);
+  }
+
+  int counter1 = 0;
+  int counter2 = 0;
+  int counter3 = 0;
+
+  while(TRUE){
+//    printf("performing copy...\n");
+    if(counter1 < insertion_index-1){
+      final_string[counter1] = string1[counter2];
+      counter1++;
+      counter2++;
+    }
+    else{
+      if(final_string[counter1]){
+        break;
+      }
+      if(string2[counter3] == '\0'){
+        final_string[counter1] = string1[counter2];
+        counter1++;
+        counter2++;
+      }
+      else{
+        final_string[counter1] = string2[counter3];
+        counter1++;
+        counter3++;
+      }
+    }
+  }
+
   printf("\n");
-  printf("%s",string2);
+  printf("%s",final_string);
   printf("\n");
-  printf("%d",insertion_index);
-  printf("\n");
+
+
   return 0;
+}
+
+int get_str_len(char * string){
 
 }
